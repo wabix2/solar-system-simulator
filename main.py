8a878d433f99f680b85b7aad63f1c32070224d93
wabix2 <wabitafese9@gmail.com>
2025-08-15 17:54:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,87 +1,103 @@
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-import webbrowser
-import threading
+import plotly.graph_objects as go
 
-st.set_page_config(page_title="3D Solar System", layout="wide")
-st.title("🌞 3D Animated Solar System")
+st.set_page_config(page_title="3D Solar System Simulator", layout="wide")
+st.title("🌞 Interactive 3D Solar System Simulator")
 
-# Automatically open browser
-def open_browser():
-    webbrowser.open("http://localhost:8501")
+# ---------------------
+# User controls
+# ---------------------
+speed = st.slider("Simulation Speed", min_value=0.01, max_value=0.1, value=0.02, step=0.01)
+show_trails = st.checkbox("Show Planet Trails", value=True)
 
-threading.Timer(1, open_browser).start()
-
-# Planet data: [semi-major axis, semi-minor axis, size, speed, color]
+# ---------------------
+# Planet data: [semi-major axis, semi-minor axis, size, color]
+# ---------------------
 planets = {
-    "Mercury": [0.4, 0.38, 80, 0.04, 'grey'],
-    "Venus": [0.7, 0.69, 100, 0.03, 'orange'],
-    "Earth": [1.0, 0.99, 120, 0.02, 'blue'],
-    "Mars": [1.5, 1.48, 90, 0.015, 'red'],
-    "Jupiter": [2.2, 2.15, 200, 0.01, 'brown'],
-    "Saturn": [2.8, 2.7, 180, 0.008, 'gold'],
-    "Uranus": [3.4, 3.35, 160, 0.006, 'lightblue'],
-    "Neptune": [4.0, 3.95, 160, 0.005, 'darkblue']
+    "Mercury": [0.4, 0.38, 8, 'grey'],
+    "Venus": [0.7, 0.69, 12, 'orange'],
+    "Earth": [1.0, 0.99, 14, 'blue'],
+    "Mars": [1.5, 1.48, 10, 'red'],
+    "Jupiter": [2.2, 2.15, 20, 'brown'],
+    "Saturn": [2.8, 2.7, 18, 'gold'],
+    "Uranus": [3.4, 3.35, 16, 'lightblue'],
+    "Neptune": [4.0, 3.95, 16, 'darkblue']
 }
 
-# Setup figure
-fig = plt.figure(figsize=(8, 8))
-ax = fig.add_subplot(111, projection='3d')
-ax.set_facecolor("black")
-ax.set_xlim(-5, 5)
-ax.set_ylim(-5, 5)
-ax.set_zlim(-1, 1)
-ax.grid(False)
-ax.set_xticks([])
-ax.set_yticks([])
-ax.set_zticks([])
+# Number of frames for the animation
+num_frames = 300
 
-# Draw Sun
-sun = ax.scatter([0], [0], [0], s=400, color='yellow')
-
-# Initialize planets and trails
-planet_points = {}
-trail_lines = {}
-trail_history = {}
+# ---------------------
+# Precompute planet positions
+# ---------------------
+planet_positions = {}
 for planet, data in planets.items():
-    x, y, z = 0, 0, 0
-    planet_points[planet] = ax.scatter([x], [y], [z], s=data[2], color=data[4], label=planet)
-    trail_lines[planet], = ax.plot([], [], [], color=data[4], linewidth=1, alpha=0.5)
-    trail_history[planet] = [[], [], []]
+    a, b, size, color = data
+    x = a * np.cos(speed * np.arange(num_frames) * 2*np.pi)
+    y = b * np.sin(speed * np.arange(num_frames) * 2*np.pi)
+    z = np.zeros(num_frames)
+    planet_positions[planet] = (x, y, z)
 
-# Animation update function
-def update(frame):
-    ax.view_init(elev=30, azim=frame*0.3)  # rotating camera
-    for planet, data in planets.items():
-        a, b, size, speed, color = data
-        x = a * np.cos(speed * frame)
-        y = b * np.sin(speed * frame)
-        z = 0.05 * np.sin(speed * frame * 2)
+# ---------------------
+# Create Plotly frames
+# ---------------------
+frames = []
+for i in range(num_frames):
+    data = []
+    # Sun
+    sun = go.Scatter3d(
+        x=[0], y=[0], z=[0],
+        mode='markers',
+        marker=dict(size=35, color='yellow'),
+        name='Sun',
+        showlegend=False
+    )
+    data.append(sun)
+    
+    # Planets
+    for planet, (x_arr, y_arr, z_arr) in planet_positions.items():
+        planet_data = go.Scatter3d(
+            x=[x_arr[i]], y=[y_arr[i]], z=[z_arr[i]],
+            mode='markers+lines' if show_trails else 'markers',
+            line=dict(width=2, color=planets[planet][3]) if show_trails else None,
+            marker=dict(size=planets[planet][2], color=planets[planet][3]),
+            name=planet,
+            showlegend=True
+        )
+        if show_trails:
+            planet_data['x'] = x_arr[:i+1]
+            planet_data['y'] = y_arr[:i+1]
+            planet_data['z'] = z_arr[:i+1]
+        data.append(planet_data)
+    
+    frames.append(go.Frame(data=data, name=str(i)))
 
-        # Update planet position
-        planet_points[planet]._offsets3d = ([x], [y], [z])
+# Initial frame
+initial_data = frames[0].data
 
-        # Update trail
-        trail_history[planet][0].append(x)
-        trail_history[planet][1].append(y)
-        trail_history[planet][2].append(z)
-        trail_lines[planet].set_data(trail_history[planet][0], trail_history[planet][1])
-        trail_lines[planet].set_3d_properties(trail_history[planet][2])
+# ---------------------
+# Layout with play button
+# ---------------------
+fig = go.Figure(
+    data=initial_data,
+    layout=go.Layout(
+        scene=dict(
+            xaxis=dict(showbackground=False),
+            yaxis=dict(showbackground=False),
+            zaxis=dict(showbackground=False),
+            aspectmode='data'
+        ),
+        updatemenus=[dict(
+            type='buttons',
+            showactive=False,
+            buttons=[dict(label='Play',
+                          method='animate',
+                          args=[None, dict(frame=dict(duration=50, redraw=True), fromcurrent=True)])]
+        )]
+    ),
+    frames=frames
+)
 
-        # Limit trail length for performance
-        if len(trail_history[planet][0]) > 150:
-            trail_history[planet][0].pop(0)
-            trail_history[planet][1].pop(0)
-            trail_history[planet][2].pop(0)
-    return list(planet_points.values()) + list(trail_lines.values()) + [sun]
-
-# Create animation
-ani = FuncAnimation(fig, update, frames=np.arange(0, 628, 2), interval=40, blit=False)
-
-# Display animation in Streamlit as GIF
-from matplotlib.animation import PillowWriter
-ani.save("solar_system.gif", writer=PillowWriter(fps=20))
-st.image("solar_system.gif", use_column_width=True)
+# Display interactive 3D simulator
+st.plotly_chart(fig, use_container_width=True)
